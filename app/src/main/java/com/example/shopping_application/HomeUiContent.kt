3b7196fa8bf2775ec9d6e915b385837d553bf776
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2025-04-12 16:28:17 +0530

    Shopping

---
+++ app/src/main/java/com/example/shopping_application/HomeUiContent.kt
@@ -0,0 +1,21 @@
+package com.example.shopping_application
+
+import androidx.annotation.DrawableRes
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun homeUI(){}
+
+@Composable
+fun button(
+    @DrawableRes icon : Int,
+    tint: Color = Color.Unspecified,
+){
+    IconButton(onClick = { /*TODO*/ }) {
+        
+    }
+    
+}
